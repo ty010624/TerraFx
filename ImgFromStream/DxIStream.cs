14b14ff6ce1d094f0c6a61c60850d4c076e8c9b2
Kelly <ty010624@hotmail.com>
2023-11-07 12:33:44 -0800

    Add files via upload

---
+++ ImgFromStream/DxIStream.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using TerraFX.Interop;
+using TerraFX.Interop.Windows;
+using static TerraFX.Interop.Windows.E;
+using static TerraFX.Interop.Windows.S;
+
+namespace Claron.WIF.Dx {
+
+  public unsafe struct IStreamImpl : IStream.Interface {
+    
+    private Stream _stream;
+
+    public static IStreamImpl* Create(Stream stream) {
+      IStreamImpl* result = (IStreamImpl*)NativeMemory.Alloc((nuint)sizeof(IStreamImpl*));
+      *result = new IStreamImpl(stream);
+      return result;
+    }
+
+    internal IStreamImpl(Stream stream) {
+      _stream = stream;
+    }
+
+    #region Core APIs (the bare minimum we need to wrap System.IO.Stream as IStream)
+
+    public HRESULT Read(void* pv, uint cb, uint* pcbRead) {
+      if (pv == null) {
+        return E_INVALIDARG;
+      }
+      
+      int bytesRead = _stream.Read(new Span<byte>(pv, (int)cb));
+      if (pcbRead != null) {
+        *pcbRead = (uint)bytesRead;
+      }
+
+      return S_OK;
+    }
+
+    public HRESULT Seek(LARGE_INTEGER dlibMove, uint dwOrigin, ULARGE_INTEGER* plibNewPosition) {
+      long newPosition;
+
+      switch ((STREAM_SEEK)dwOrigin) {
+        case STREAM_SEEK.STREAM_SEEK_SET:
+          newPosition = dlibMove.QuadPart;
+          break;
+
+        case STREAM_SEEK.STREAM_SEEK_CUR:
+          newPosition = _stream.Position + dlibMove.QuadPart;
+          break;
+
+        case STREAM_SEEK.STREAM_SEEK_END:
+          newPosition = _stream.Length + dlibMove.QuadPart;
+          break;
+
+        default:
+          return E_INVALIDARG;
+      }
+
+      if (newPosition < 0 || newPosition > _stream.Length) {
+        return E_INVALIDARG; 
+      }
+
+      try {
+        _stream.Seek(newPosition, SeekOrigin.Begin);
+
+        if (plibNewPosition != null) {
+          plibNewPosition->QuadPart = (ulong)newPosition;
+        }
+
+        return S_OK;
+      }
+      catch (Exception) {
+        return E_FAIL; 
+      }
+    }
+
+    public HRESULT Write(void* pv, uint cb, uint* pcbWritten) => E_NOTIMPL;
+
+    #endregion
+
+    #region Helper APIs
+
+    public HRESULT QueryInterface(Guid* riid, void** ppvObject) => E_NOTIMPL;
+
+    public uint AddRef() => 0; 
+
+    public uint Release() => 0; 
+
+    public HRESULT SetSize(ULARGE_INTEGER libNewSize) => E_NOTIMPL;
+
+    public HRESULT CopyTo(IStream* pstm, ULARGE_INTEGER cb, ULARGE_INTEGER* pcbRead, ULARGE_INTEGER* pcbWritten) => E_NOTIMPL;
+
+    public HRESULT Commit(uint grfCommitFlags) => E_NOTIMPL;
+
+    public HRESULT Revert() => E_NOTIMPL;
+
+    public HRESULT LockRegion(ULARGE_INTEGER libOffset, ULARGE_INTEGER cb, uint dwLockType) => E_NOTIMPL;
+
+    public HRESULT UnlockRegion(ULARGE_INTEGER libOffset, ULARGE_INTEGER cb, uint dwLockType) => E_NOTIMPL;
+
+    public HRESULT Stat(STATSTG* pstatstg, uint grfStatFlag) => E_NOTIMPL;
+
+    public HRESULT Clone(IStream** ppstm) => E_NOTIMPL;
+
+    #endregion
+
+    static Guid* INativeGuid.NativeGuid => throw new NotImplementedException();
+
+  }
+}
---
+++ ImgFromStream/Program.cs
@@ -0,0 +1,166 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Claron.WIF.Dx;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using TerraFX.Interop.Windows;
+using static TerraFX.Interop.Windows.Windows;
+
+Console.WriteLine("Hello, World!");
+
+unsafe
+{
+    var FactoryImage = CreateWicImageFactory();
+
+    // Reading from file directly is working fine
+    // It reads from embedded resource (make sure the embedded resource is good), save into a .png then load from file.
+    // This runs to the end. In our application it successfully returns the bitmap handle.
+    // CreateBitmapFromFile(FactoryImage);
+
+    // Reading from stream is failing...
+    // It reads from embedded resource as a stream, but this always crashes at CreateDecoderFromStream()
+    CreateBitmapFromStream(FactoryImage);
+}
+
+unsafe IWICImagingFactory* CreateWicImageFactory()
+{
+    IWICImagingFactory* wicImageFactory = null;
+
+    Guid clsidWICImagingFactory = CLSID.CLSID_WICImagingFactory;
+    ThrowIfFailed(CoCreateInstance(
+      &clsidWICImagingFactory, null,
+      (uint)CLSCTX.CLSCTX_INPROC_SERVER,
+      __uuidof<IWICImagingFactory>(),
+      (void**)&wicImageFactory));
+
+    return wicImageFactory;
+}
+
+unsafe void CreateBitmapFromFile(IWICImagingFactory* FactoryImage)
+{
+    //ID2D1Bitmap* dBitmap;
+
+    using ComPtr<IWICBitmapDecoder> wicDecoder = null;
+    using ComPtr<IWICBitmapFrameDecode> wicFrame = null;
+    using ComPtr<IWICBitmapSource> wicFrameBitmapSource = null;
+    using ComPtr<IWICFormatConverter> wicConverter = null;
+    using ComPtr<IWICBitmapSource> wicConverterBitmapSource = null;
+
+    var pngName = "ToothSolid";
+    var assembly = Assembly.GetExecutingAssembly();
+    var pngResourceName = assembly.GetManifestResourceNames().FirstOrDefault(str => str.Contains(pngName));
+
+    if (string.IsNullOrEmpty(pngResourceName))
+    {
+        throw new Exception($"Resource {pngName} not found.");
+    }
+
+    // Temporarily save a local copy
+    var assemblyPath = assembly.Location;
+    var directory = Path.GetDirectoryName(assemblyPath);
+    string filePathToSave = @$"{directory}\{pngName}.png";
+    if (!File.Exists(filePathToSave))
+    {
+        using (var imgStream = assembly.GetManifestResourceStream(pngResourceName))
+        {
+            if (imgStream != null)
+            {
+                using (var fileStream = File.Create(filePathToSave))
+                {
+                    imgStream.CopyTo(fileStream);
+                }
+            }
+        }
+    }
+
+    IntPtr filePathPtr = Marshal.StringToHGlobalUni(filePathToSave);
+    ThrowIfFailed(FactoryImage->CreateDecoderFromFilename(
+      (ushort*)filePathPtr,
+      null,
+      GENERIC_READ,
+      WICDecodeOptions.WICDecodeMetadataCacheOnLoad,
+      wicDecoder.GetAddressOf()));
+
+    ThrowIfFailed(wicDecoder.Get()->GetFrame(0, wicFrame.GetAddressOf()));
+    ThrowIfFailed(wicFrame.Get()->QueryInterface(__uuidof<IWICBitmapSource>(), (void**)&wicFrameBitmapSource));
+
+    FactoryImage->CreateFormatConverter(wicConverter.GetAddressOf());
+    Guid clsidWICPixelFormat = GUID.GUID_WICPixelFormat32bppPBGRA;
+    wicConverter.Get()->Initialize(
+      wicFrameBitmapSource,
+      &clsidWICPixelFormat,
+      WICBitmapDitherType.WICBitmapDitherTypeNone,
+      null, 0.0,
+      WICBitmapPaletteType.WICBitmapPaletteTypeCustom);
+    ThrowIfFailed(wicConverter.Get()->QueryInterface(__uuidof<IWICBitmapSource>(), (void**)&wicConverterBitmapSource));
+
+    //ThrowIfFailed(DeviceContext2D->CreateBitmapFromWicBitmap(wicConverterBitmapSource, null, &dBitmap));
+
+    if (filePathPtr != IntPtr.Zero)
+    {
+        Marshal.FreeHGlobal(filePathPtr);
+    }
+
+    //return dBitmap;
+}
+
+unsafe void CreateBitmapFromStream(IWICImagingFactory* FactoryImage)
+{
+    //ID2D1Bitmap* dBitmap = null;
+
+    using ComPtr<IWICBitmapDecoder> wicDecoder = null;
+    using ComPtr<IWICBitmapFrameDecode> wicFrame = null;
+    using ComPtr<IWICBitmapSource> wicFrameBitmapSource = null;
+    using ComPtr<IWICFormatConverter> wicConverter = null;
+    using ComPtr<IWICBitmapSource> wicConverterBitmapSource = null;
+
+    var pngName = "ToothSolid";
+    var assembly = Assembly.GetExecutingAssembly();
+    var pngResourceName = assembly.GetManifestResourceNames().FirstOrDefault(str => str.Contains(pngName));
+
+    if (string.IsNullOrEmpty(pngResourceName))
+    {
+        throw new Exception($"Resource {pngName} not found.");
+    }
+
+    using (var imgStream = assembly.GetManifestResourceStream(pngResourceName))
+    {
+        if (imgStream == null)
+        {
+            throw new Exception("Image resource not found.");
+        }
+
+        var ptrStream = IStreamImpl.Create(imgStream);
+
+        try
+        {
+            ThrowIfFailed(FactoryImage->CreateDecoderFromStream(
+                (IStream*)ptrStream,
+                null,
+                WICDecodeOptions.WICDecodeMetadataCacheOnLoad,
+                wicDecoder.GetAddressOf()));
+
+            ThrowIfFailed(wicDecoder.Get()->GetFrame(0, wicFrame.GetAddressOf()));
+            ThrowIfFailed(wicFrame.Get()->QueryInterface(__uuidof<IWICBitmapSource>(), (void**)&wicFrameBitmapSource));
+            ThrowIfFailed(FactoryImage->CreateFormatConverter(wicConverter.GetAddressOf()));
+
+            Guid clsidWICPixelFormat = GUID.GUID_WICPixelFormat32bppPBGRA;
+            wicConverter.Get()->Initialize(
+                wicFrameBitmapSource,
+                &clsidWICPixelFormat,
+                WICBitmapDitherType.WICBitmapDitherTypeNone,
+                null, 0.0,
+                WICBitmapPaletteType.WICBitmapPaletteTypeCustom);
+            ThrowIfFailed(wicConverter.Get()->QueryInterface(__uuidof<IWICBitmapSource>(), (void**)&wicConverterBitmapSource));
+            //ThrowIfFailed(DeviceContext2D->CreateBitmapFromWicBitmap(wicConverterBitmapSource, null, &dBitmap));
+        }
+        finally
+        {
+            if (ptrStream != null)
+                ptrStream->Release();
+        }
+    }
+
+    //return dBitmap;
+}
+
+
